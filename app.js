74b853e66df1a580d1ec1037b2cf78565a028644
Charlie Smith <mehta.rohan@outlook.com>
2018-07-07 17:40:38 -0400

    git commit -m "VueJS Components are Working. Join Lobby = Good"

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 const express = require("express")
 const app = express()
+const http = require("http").Server(app)
+const io = require("socket.io")(http)
 
 app.use(express.static("statics"))
-
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(3000, ()=> console.log("The website is running"))
+class Lobby{
+    constructor(){
+        this.questions = []
+        this.users = []
+        // Generate Unique Code
+        var foundUnique = false;
+        while(!foundUnique){
+            foundUnique = true;
+            this.roomCode = [Math.floor(Math.random()*10), 
+                Math.floor(Math.random()*10), 
+                Math.floor(Math.random()*10), 
+                Math.floor(Math.random()*10), 
+                Math.floor(Math.random()*10)]
+            // Checks to make sure the generated code is unique
+            for(var i =0; i < lobbys.length; i++){
+                if (this.roomCode == lobbys[i].roomCode){
+                    foundUnique = false;
+                    console.log("Generated Code")
+                }
+            }
+            
+        }
+        // Convert code to string for proper room handling
+        this.roomCode = this.roomCode.join('')
+        
+    }
+
+    addUser(username, socket){
+        this.users.push({"socket": socket.id, "username": username})
+    }
+}
+var lobbys = []
+function findLobbyByCode(code){
+    for(var i =0; i < lobbys.length; i++){
+        if (code == lobbys[i].roomCode){
+            return lobby[i]
+        }
+    }
+}
+
+io.on("connection", (socket)=>{
+    socket.on("RequestRoom", (data) => {
+        if (data["roomCode"] != null) {
+            // User is joining an existing room number
+            socket.join(data["roomCode"])
+        }else{
+            // User wants to create a lobby
+            var lobby = new Lobby()
+            lobbys.push(lobby)
+            socket.join(lobby.code)            
+        }
+        findLobbyByCode(data["roomCode"], socket)
+        io.sockets.in(lobby.code).emit("New Opponent")
+    })
+})
+
+http.listen(3000, ()=> console.log("The website is running"))
--- statics/js/index.js
+++ statics/js/index.js
@@ -1,18 +1,43 @@
 var socket = null;
+Vue.component("join-lobby", {
+    template: `
+    <div>
+        <input v-model="username" placeholder="Username" type="text" />
+        <input v-model="roomNumber" placeholder="Will automatically be created if this space is blank" type="number" />
+        <button @click="newServerRequest">Create Server</button>
+    </div>
+    `,
+
+    data: function (){
+        return {
+            roomNumber: null,
+            username: null,
+            
+            newServerRequest: function(){
+                socket.emit("RequestRoom",{"code":this.roomNumber, "username": this.username});
+            }
+        }
+    }
+})
+
 var app = new Vue({
     el: "#app",
     data: {
         message: "yoyo",
+        state: "join-lobby"
     },
     methods: {
-        
+        // Functions to Handle Component Insertion
+        lobbyJoin() {
+            if (this.state == "join-lobby"){return true}
+        }
     },
     created: function(){
         socket = io();
     },
     mounted: function(){
-        socket.on("", ()=>{
-            
+        socket.on("New Opponent", ()=>{
+            console.log("New person")
         })
     }
 });
